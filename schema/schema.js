17c6254c28be99c99476c0758150cc1eb3305464
Renan Pallin <renanpallin@gmail.com>
2019-04-15 22:17:06 -0300

    CompnyType

--- schema/schema.js
+++ schema/schema.js
@@ -4,10 +4,14 @@ const axios = require('axios');
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
-const users = [
-	{ id: '23', firstName: 'Bill', age: 20 },
-	{ id: '47', firstName: 'Samantha', age: 21 },
-];
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+	},
+});
 
 const UserType = new GraphQLObjectType({
 	name: 'User',
@@ -15,6 +19,14 @@ const UserType = new GraphQLObjectType({
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
+		company: {
+			type: CompanyType,
+			resolve(parentValue, args) {
+				return axios
+					.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+					.then(res => res.data);
+			},
+		},
 	},
 });
 
